c319f690f992d085e614064c775a111d46a4bbcb
Tal <artishok.tal@gmail.com>
2022-02-23 16:31:30 +0200

    deugging

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,9 +1,10 @@
 import {storage} from './storage.service.js'
 export const locService = {
     getLocs,
-    addLoc
+    addLoc,
 }
 
+hi()
 
 const LOCS_KEY = 'LOCS'
 const locs = storage.load(LOCS_KEY) || []; 
@@ -12,6 +13,13 @@ function addLoc(loc){
     loc.createdAt = Date.now(); 
     locs.push(loc);
     storage.save(LOCS_KEY, locs)
+}
+
+
+function hi(){
+    fetch('https://maps.googleapis.com/maps/api/geocode/json?address=Israel&key=AIzaSyD-KPvaSQmdmLMPid-c2TdBzO8d_kLjUVE')
+    .then(res => res.json())
+    .then(res => console.log(res))
 }
 
 function getLocs() {
