1b0670b1001888f8535752c3eeeb1fdcab278603
Tal <artishok.tal@gmail.com>
2022-02-23 14:24:31 +0200

    added api key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -38,7 +38,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyD-KPvaSQmdmLMPid-c2TdBzO8d_kLjUVE'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
